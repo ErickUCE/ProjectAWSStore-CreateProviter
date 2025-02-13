d3bb3588793e319d4bf98196463693ca21a74c1b
erickuce1509 <erickuce1509@gmail.com>
2025-02-13 18:51:32 -0500

    CI/CD para CreateProvider en AWS EC2 Estatics ips

--- src/resolvers.js
+++ src/resolvers.js
@@ -10,9 +10,9 @@ const resolvers = {
 
                 // Notificar a los otros microservicios
                 const instances = [
-                    'http://localhost:5001/sync-create', // Microservicio de Eliminar
-                    'http://localhost:5002/sync-create',  // Microservicio de Update
-                    'http://localhost:5003/sync-create'  // ✅ Microservicio de Leer
+                    'http://23.21.70.193:5001/sync-create', // Microservicio de Eliminar
+                    'http://52.5.181.183:5002/sync-create',  // Microservicio de Update
+                    'http://3.229.198.244:5003/sync-create'  // ✅ Microservicio de Leer
                 ];
 
                 for (const instance of instances) {
